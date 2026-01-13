fb28214f927dd2cf8de8c6d1202ebeb0df5059b4
Dr Ian M. Carr <msjimc@leeds.ac.uk>
2026-01-13 15:58:56 +0000

    Add project files.

---
+++ CreateGitHubContentsSection/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CreateGitHubContentsSection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnmake = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbIgnore = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboList = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.cboList);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lbIgnore);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnmake);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnSelect);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(449, 318);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File selection";
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnQuit.Location = new System.Drawing.Point(12, 338);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelect.Location = new System.Drawing.Point(374, 19);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press the \'Select\' button to select the base folder of the GitHub repository";
+            // 
+            // btnmake
+            // 
+            this.btnmake.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnmake.Location = new System.Drawing.Point(368, 289);
+            this.btnmake.Name = "btnmake";
+            this.btnmake.Size = new System.Drawing.Size(75, 23);
+            this.btnmake.TabIndex = 4;
+            this.btnmake.Text = "Make";
+            this.btnmake.UseVisualStyleBackColor = true;
+            this.btnmake.Click += new System.EventHandler(this.btnmake_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(308, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Press \'Make\' to create a file that contains the  contents sections";
+            // 
+            // lbIgnore
+            // 
+            this.lbIgnore.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbIgnore.FormattingEnabled = true;
+            this.lbIgnore.Location = new System.Drawing.Point(9, 78);
+            this.lbIgnore.Name = "lbIgnore";
+            this.lbIgnore.Size = new System.Drawing.Size(440, 199);
+            this.lbIgnore.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Select to add to list";
+            // 
+            // cboList
+            // 
+            this.cboList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboList.FormattingEnabled = true;
+            this.cboList.Location = new System.Drawing.Point(105, 48);
+            this.cboList.Name = "cboList";
+            this.cboList.Size = new System.Drawing.Size(264, 21);
+            this.cboList.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(374, 46);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 373);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Make GitHub contents sections";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnmake;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.CheckedListBox lbIgnore;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ComboBox cboList;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CreateGitHubContentsSection/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateGitHubContentsSection
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFolder = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            string folder = FileString.GetFolder("Select Folder Containing Files to Process", "");
+            if (System.IO.Directory.Exists(folder) == false) { return; }
+            List<string> files= new List<string>();
+
+            ProcessThisFolder(folder, files);
+            files.Sort();
+
+            int len = folder.Length + 1;
+            lbIgnore.Items.Clear();
+            cboList.Items.Clear();
+            cboList.Items.Add("Select");
+            foreach (string file in files)
+            {cboList.Items.Add(file.Substring(len)); }
+            cboList.SelectedIndex = 0;
+            selectedFolder = folder + "\\";
+        }
+
+        private void ProcessThisFolder(string folder, List<string> files)
+        {
+            string[] allFiles = System.IO.Directory.GetFiles(folder, "*.md");
+            foreach (string file in allFiles)
+            {
+                files.Add(file);
+            }
+
+            foreach(string aFolder in System.IO.Directory.GetDirectories(folder))
+            {
+                ProcessThisFolder(aFolder, files);
+            }
+        }
+
+        private void btnmake_Click(object sender, EventArgs e)
+        {
+            List<string> titles = new List<string>();
+
+            for(int index = 0; index < lbIgnore.Items.Count; index++)
+            {
+                if (lbIgnore.CheckedIndices.Contains(index) == false)
+                {
+                    string file = selectedFolder + lbIgnore.Items[index].ToString();
+                    string fileNmae = lbIgnore.Items[index].ToString();
+                    titles.Add("   ");
+                    titles.Add("## " + fileNmae.Substring(0,fileNmae.Length-3));
+                    titles.Add("## List of contents");
+                    titles = getTitles(file, titles, lbIgnore.Items[index].ToString());                    
+                }
+            }
+
+            System.IO.StreamWriter sw = null;
+            try 
+            {
+                sw = new System.IO.StreamWriter(selectedFolder + "ContentsSection.md", false);
+                foreach(string title in titles)
+                {
+                    sw.WriteLine(title);
+                }
+
+            }
+            catch (Exception ex)
+            { MessageBox.Show("Error: " + ex.Message); }
+            finally
+            {
+                if (sw != null)
+                { sw.Close(); }
+            }
+        }
+
+        private List<string> getTitles(string file, List<string> titles, string path)
+        {
+           string[] lines = System.IO.File.ReadAllLines(file);
+            foreach(string line in lines)
+            {
+                if (line.Trim().StartsWith("#"))
+                {
+                    string title = line.Trim();
+                    string link = title.Replace("_", "").Replace("*", "");
+                    int len = 0;
+                    while (len != link.Length)
+                    {
+                        link = link.Replace("  ", " ");
+                        len = link.Length;
+                    }
+
+                    int hashCount = 0;
+                    for(int index = 0; index < link.Length; index++)
+                    {
+                        if (link[index] == '#')
+                        { hashCount++; }
+                        else
+                        { break; }
+                    }
+
+                    hashCount = (hashCount - 1) * 2;
+                    string space = new string(' ', hashCount) + "- ";
+                    link = CleanLink(link).Trim();
+                    link = "#" + link.Replace(" ", "-");
+                    title = title.Replace("# ", "");
+                    title = title.Replace("#", "");
+                    titles.Add(space + "[" + title + "](" + path +  link + ")" );
+                }
+            }
+            return titles;
+        }
+
+        private string CleanLink(string link)
+        {
+            string answer = "";
+            foreach(char c in link)
+            {
+                if (char.IsLetterOrDigit(c) == true || c == ' ' || c == '-')
+                { answer += c; }
+            }
+            return answer.ToLower();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cboList.SelectedIndex > 0)
+            { lbIgnore.Items.Add(cboList.SelectedItem.ToString()); }
+        }
+    }
+}
